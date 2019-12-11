39ddd123fddbecae595a73713b85dd4606a757c5
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2019-12-11 17:27:54 +0530

    made implementation simple

--- arpReply/arp_reply.c
+++ arpReply/arp_reply.c
@@ -9,20 +9,23 @@
 #include <linux/if_arp.h>
 #include <linux/in.h>
 #include <linux/ether.h>
-
+#define BUFFER 60
+#define ETH_LEN 14
 static int sendData(struct net_device* dev,uint8_t dest_addr[ETH_ALEN],uint16_t proto);
 static struct nf_hook_ops* hookOps;
 static struct net_device* dev = 0;
-static char* srcIp = 0;
-static char* myIp = 0;
-static char* ethData = 0;
-static char* ethName = 0;
-static char* wirelessName = 0;
-static char* wirelessData = 0;
-module_param(myIp, charp ,0000);
-module_param(srcIp, charp ,0000);
-module_param(ethName,charp,0000);
-module_param(wirelessName,charp,0000);
+static char* data = 0;
+struct arp_header {
+    unsigned short hardware_type;
+    unsigned short protocol_type;
+    unsigned char hardware_len;
+    unsigned char protocol_len;
+    unsigned short opcode;
+    unsigned char sender_mac[6];
+    unsigned char sender_ip[4];
+    unsigned char target_mac[6];
+    unsigned char target_ip[4];
+};
 static int pton(const char* src,unsigned char* dst){
 	int saw_digit,octets,ch;
 	unsigned char* tp;
@@ -52,31 +55,38 @@ static int pton(const char* src,unsigned char* dst){
 	if(octets<4) return 0;
 	return 1;
 }
-static unsigned int hookFunction(void* priv,struct sk_buff* skf,const struct nf_hook_state* state){
-	struct iphdr* iph;
-	struct sockaddr_in saMy;
-	struct sockaddr_in saBroadcast;
-	if(!skf) return NF_ACCEPT;
-	iph = ip_hdr(skf);
-	pton(myIp,(unsigned char*)&(saMy.sin_addr.s_addr));
-	pton("255.255.255.255",(unsigned char*)&(saBroadcast.sin_addr.s_addr));
-	if(iph->daddr != saMy.sin_addr.s_addr && iph->daddr != saBroadcast.sin_addr.s_addr){
-		return NF_DROP;
+static unsigned int hookFunction(unsigned int hooknum,struct sk_buff *skb,const struct net_device *in,const struct net_device *out,int (*okfn)(struct sk_buff*)){
+	struct ethhdr* ethernet;
+	char* tmp;
+	struct arp_header* inHeader;
+	if(!skb){
+		return NF_ACCEPT;
 	}
-	return NF_ACCEPT;
+	tmp = (char*) ethernet;
+	tmp = tmp+ETH_LEN;
+	inHeader = (struct arp_header*) tmp;
+	if(inHeader->ar_op != __constant_htons(ARPOP_REQUEST)){ 
+		return NF_ACCEPT;
+	}
+	if(((in->ip_ptr)->ifa_list)->ifa_address == myIP){
+		return NF_ACCEPT;
+	}
+
 }
+
 static int init(void){
-	ethData = (char*) kcalloc(1,sizeof(struct ethhdr) + sizeof(struct arpreq));
+	data = (char*) kcalloc(1,sizeof(struct ethhdr) + sizeof(struct arpreq),GFP_KERNEL);
 	hookOps = (struct nf_hook_ops*) kcalloc(1,sizeof(struct nf_hook_ops),GFP_KERNEL);
-	hookOps->hook = (nf_hookfn*)hook_function;
+	hookOps->hook = (nf_hookfn*) hook_function;
+	hookOps->pf = NFPROTO_ARP;
 	hookOps->hooknum = NF_ARP_IN;
-	hookOps->pf = 0;
-	hookOps->priority = INT_MIN;
+	hookOps->priority = NF_IP_PRI_FIRST;
 	dev = &init_net;
 	nf_register_net_hook(&init_net,hookOps);
 }
 static void exit(void){
 	nf_unregister_net_hook(&init_net,hookOps);
+	kfree(data);
 	kfree(hookOps);
 }
 
